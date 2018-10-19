da020b8bfef065a7365009663ac5a1925178d195
Oleg Kmechak <ofcrashbashW@gmail.com>
2018-10-19 04:49:08 +0200

    improve pelicancon.py file(adopt for blue-pelican and simplified)

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,93 +1,99 @@
-# Theme-specific settings
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = 'Oleg Kmechak'
 SITENAME = 'Oleg Kmechak'
-DOMAIN = 'doweneedmath.ua'
-BIO_TEXT = 'Developer and musician from Sokal.'
-FOOTER_TEXT = 'Powered by <a href="http://getpelican.com">Pelican</a> and <a href="http://pages.github.com">GitHub&nbsp;Pages</a>.'
+SITEURL = '.'
 
-SITE_AUTHOR = 'Oleg Kmechak'
-#TWITTER_USERNAME = '@none'
-GOOGLE_PLUS_URL = 'https://plus.google.com/+OlegFedyna'
-INDEX_DESCRIPTION = 'Website and blog of Oleg Kmechak, a developer and musician from Sokal, BC.'
+# all defaults to True.
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME   = True
+DISPLAY_MENU   = True
 
-SIDEBAR_LINKS = [
-    '<a href="category/about.html">About</a>',
-    '<a href="categories.html">Categories</a>',
-    '<a href="archives.html">Archive</a>'
-]
 
-ICONS_PATH = 'images/icons'
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+EN_URL             = 'index.html'
+EN_SAVE_AS         = 'index.html'
+UA_URL             = 'ua/index.html'
+UA_SAVE_AS         = 'ua/index.html'
+ABOUT_URL          = 'category/about'
+ABOUT_SAVE_AS      = 'category/about.html'
 
-SOCIAL_ICONS = [
-    ('mailto:oleg.kmechak@gmail.com', 'Email (oleg.kmechak@gmail.com)', 'fa-envelope'),
-    #('http://twitter.com/None', 'Twitter', 'fa-twitter'),
-    ('https://github.com/ofcrashbash', 'GitHub', 'fa-github'),
-    ('https://soundcloud.com/rain_must_fall', 'SoundCloud', 'fa-soundcloud'),
-    ('https://www.facebook.com/profile.php?id=100005110871470', 'Facebook', 'fa-facebook'),
-    #('https://vk.com/rain_must_fall', 'Vkontakte', 'fa-vk'),
-    #('https://plus.google.com/+OlegFedyna', 'Google+', 'fa-google-plus'),
-    ('http://www.youtube.com/c/OlegFedyna ','Youtube', 'fa-youtube')
-    #('/atom.xml', 'Atom Feed', 'fa-rss')
-]
 
-THEME_COLOR = '#FF8000'
 
-# Pelican settings
-RELATIVE_URLS = True
-SITEURL = 'http://doweneedmath.pp.ua'
+MENU_INTERNAL_PAGES = (
+    #('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+    ('About', ABOUT_URL, ABOUT_SAVE_AS),
+    #('En', EN_URL, EN_SAVE_AS),#TODO
+    #('Укр', UA_URL, UA_SAVE_AS),#TODO!!
+    ('Tags', TAGS_URL, TAGS_SAVE_AS)
+)
+
+PELICAN_DIR = ''
+if "PELICAN" in os.environ:
+    PELICAN_DIR = os.environ["PELICAN"]
+else: 
+    print("SOMETHING WRONG!")
+    print(os.environ)
+
+
+
+THEME = PELICAN_DIR + '\\blue-penguin' #medius, waterspill, blue-penguin
+print(THEME)
+PATH = 'content'
+
 TIMEZONE = 'Europe/Kiev'
-DEFAULT_DATE = 'fs'
-DEFAULT_DATE_FORMAT = '%B %d, %Y'
-DEFAULT_PAGINATION = False
-SUMMARY_MAX_LENGTH = 70
 
-
-THEME = 'pneumatic_upd'
-
-#ARTICLE_URL = '/{category}/{slug}/'
-#ARTICLE_SAVE_AS = 'category/{slug}/' + 'index.html'
-
-#PAGE_URL = '{slug}/'
-#PAGE_SAVE_AS = PAGE_URL + 'index.html'
-
-#TODO resolve this: do i need this archives?
-#ARCHIVES_SAVE_AS = 'blog/index.html'
-#YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
-#MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
-
-# Disable authors, categories, tags, and category pages
-#DIRECT_TEMPLATES = ['index', 'blogs']
-#CATEGORY_SAVE_AS = ''
-
-# Disable Atom feed generation
-FEED_ATOM = 'atom.xml'
+# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
-TYPOGRIFY = True
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.admonition': {},
-        'markdown.extensions.codehilite': {'linenums': None},
-        'markdown.extensions.extra': {},
-    },
-    'output_format': 'html5',
-}
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
-CACHE_CONTENT = False
-DELETE_OUTPUT_DIRECTORY = True
-OUTPUT_PATH = 'develop'
-PATH = 'content'
+DEFAULT_PAGINATION = 10
 
-#templates = ['404.html']
-#TEMPLATE_PAGES = {page: page for page in templates}
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
 
-STATIC_PATHS = ['Images', 'uploads', 'extra', 'Images/hello_world']
+
+
+PLUGIN_PATHS = [PELICAN_DIR + '\\pelican-plugins']
+print(PLUGIN_PATHS)
+PLUGINS = ['render_math',  'assets']#'i18n_subsites' FIXME
+
+#multilanguage setup
+DEFAULT_LANG = 'en'
+
+STATIC_PATHS = ['Images', 'Images/hello_world', 'Images/icons', 'Files']
 IGNORE_FILES = ['.DS_Store', 'pneumatic.scss', 'pygments.css']
 
-PLUGIN_PATHS = ['C:/Users/ofcra/Programming_Files_And_Scripts/Pelican/pelican-plugins']
-PLUGINS = ['assets', 'neighbors', 'render_math', 'i18n_subsites']
+DISPLAY_PAGES_ON_MENU = True
+
+SUMMARY_MAX_LENGTH = 40
+
 ASSET_SOURCE_PATHS = ['static']
 ASSET_CONFIG = [
     ('cache', False),
@@ -96,36 +102,6 @@ ASSET_CONFIG = [
     ('versions', False),
 ]
 
-#multilanguage setup
-I18N_SUBSITES = {
-    'ua': {'SITENAME': 'Олег Кмечак',
-           'BIO_TEXT': 'Програміст і музикант зі Сокаля',
-           'FOOTER_TEXT': 'Побудоване на <a href="http://getpelican.com">Pelican</a> і <a href="http://pages.github.com">GitHub&nbsp;Pages</a>.',
-           'SITE_AUTHOR': 'Oleg Kmechak',
-           'INDEX_DESCRIPTION': 'Мій вебсайт і блог, програміст і музикант зі Cокаля.',
-           'SIDEBAR_LINKS': [
-                '<a href="category/about.html">Про</a>',
-                '<a href="categories.html">Блог</a>',
-                '<a href="archives.html">Архів</a>'
-                ]
-            }
-    }
-
-DEFAULT_LANG = 'en'
-I18N_UNTRANSLATED_ARTICLES = 'keep'
-I18N_UNTRANSLATED_PAGES = 'keep'
-
-UA_LINK_NAME = 'Укр'
-EN_LINK_NAME = 'En'
-
-languages_lookup = {
-    'ua': 'Укр',
-    'en': 'En',
-    }
-
-def lookup_lang_name(lang_code):
-    return languages_lookup[lang_code]
-
-JINJA_FILTERS = {
-    'lookup_lang_name': lookup_lang_name
-    }
+CACHE_CONTENT = False
+DELETE_OUTPUT_DIRECTORY = True
+THEME_STATIC_DIR = 'theme'
