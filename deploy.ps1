32b710072cffb10df5cd5a167e0073be0168224f
Oleg Kmechak <ofcrashbashW@gmail.com>
2018-10-29 02:28:06 +0100

    add delpoy.ps1 script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,10 @@
 echo "Deployment to GitHUB"
 
 # Build the project
-hugo -t "material-blog"
-cd public
+hugo -D -t "material-blog"
+cd okmechak-github-io
+git add .
+git commit -m "rebuild site `date` "
+git push origin master
+cd ..
 # TODO continue this script
